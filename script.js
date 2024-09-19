3e2760d932f1fc3df4f502757d200c7c6ac5b9d9
Mass-hideo <massimo.viviano09@myhunter.cuny.edu>
2024-09-19 13:57:49 -0400

    Update script.js
    added bounce

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-window.onload = function () {
-    const bars = document.querySelectorAll(".bar");
+window.onload = function () { //an animation to occur when the page loads
+    const bars = document.querySelectorAll(".bar"); //create an element that grabs all the elements with the class bar
     gsap.from(bars, {
       duration: .5,
       opacity: 0,
